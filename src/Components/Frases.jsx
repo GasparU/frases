c04833d1b216b3881719ed5ab8b18210d5c44067
Jose Gaspar <sainc2016@gmail.com>
2023-03-28 10:20:17 -0500

    terminado sin movimiento 3d

--- src/Components/Frases.jsx
+++ src/Components/Frases.jsx
@@ -1,11 +1,4 @@
-
 export default function Frases({ valor }) {
-    // VanillaTilt.init(document.querySelector(".App__text"), {
-    //     max: 25,
-    //     speed: 400,
-    //     glare: true,
-    //     "max-glare": 1,
-    // });
     return (
         <>
             <p className='App__text'>{valor.phrase}<br />
