0acc9f8c3fb50fee65a12571597fdf277d7d9787
Jose Gaspar <sainc2016@gmail.com>
2023-03-28 09:41:04 -0500

    particulas

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Frases from './Components/Frases'
 import phrases from "./utils/phrases.json"
 import { useState } from 'react';
 import LogicPhrase from './Components/Logic';
-// import Particle from "./utils/Particle";
+import Particle from "./utils/Particle";
 const imgs = [1, 2, 3, 4, 5, 6, 7, 8]
 function App() {
   const [valor, setValor] = useState(LogicPhrase(phrases))
@@ -14,7 +14,7 @@ function App() {
   }
   return (
     <div style={appStyle} className="App2">
-      {/* <Particle /> */}
+      <Particle />
       <h1 className="App__title">F<span>rases</span> Cé<span>lebres</span></h1>
       <Button setValor={setValor} setNumberImg={setNumberImg} />
       <Frases valor={valor} />
